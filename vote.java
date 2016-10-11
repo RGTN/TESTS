e867118fb976b440e431c4f0270113da2e239a84
roadkillsanta <ericnie7@gmail.com>
2016-10-11 14:42:53 -0400

    Create vote.java

---
+++ vote.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class votemethod{
+	public static String calc(ArrayList v){
+		for(int i=0; i<v.get(0); i++){
+			String num="choice"+i;
+			String Choice1=v.get(num);
+		}
+	}
+	public static void main(String args[]){
+		Scanner input=new Scanner();
+	}
+}
